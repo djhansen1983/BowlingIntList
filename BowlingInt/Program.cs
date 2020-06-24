438216a05bcc8d95b1b30b97f1975f57a9f28bd5
djhansen <david.j.hansen@gmail.com>
2020-06-24 15:04:49 -0400

    Add project files.

---
+++ BowlingInt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BowlingInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunBowling();
+        }
+        
+            
+        
+        static void RunBowling()
+        {
+            Random rnd = new Random();
+            var frames = new List<int>(10);
+            for(var idx = 0; idx < 10; idx++)
+            {
+                var score = rnd.Next(0, 31);
+                frames.Add(score);
+            }
+            var total = 0;
+            foreach(var frame in frames)
+            {
+                total += frame;
+            }
+            Console.WriteLine($"Score is {total}");
+        }
+        
+        
+
+        
+    }
+}
